c554c146fbdc43f028eff1c4da538a00945de21a
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-04-24 09:46:46 -0300

    Create mytime.c

---
+++ SEMANA02/Exercicio03/mytime.c
@@ -0,0 +1,19 @@
+#ifndef MY_TIME_H
+#define MY_TIME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+// medindo tempo em C (Linux)
+typedef struct timeval timer; //adicionando um novo nome para o tipo struct timeval
+
+// função que marca o tempo atuaç
+timer tic();
+
+//marca o final do trecho do tempo;
+timer tac();
+
+float compTime(timer tic, timer tac);
+
+#endif
