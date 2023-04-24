c86fdfd318a38d4f012224da1fc94fbe3c56059d
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-04-24 11:18:02 -0300

    Create py03.py

---
+++ SEMANA02/Exercicio04/py03.py
@@ -0,0 +1,26 @@
+# Operadores aritimétimos
+# Soma: 3+2
+# Subtração: 3-2
+# Multiplicação: 3*2
+# Divisão: 3/2
+# Divisão (só mostra o número inteiro): 3//2
+# Exponenciação: 3**2
+# Resto da divisão: 3%2
+# Módulo: abs()
+# Arredondar: round()
+
+#Comparação
+#Igual: 3==2
+#Diferente: 3!=2
+#Maior que: 3>2
+#Menor que: 3<2
+#Maior ou igual: 3>=2
+#Menor ou igual: 3<=2
+
+num_1='100'
+num_2='200'
+
+
+num_1=int(num_1)
+num_2=int(num_2)
+print(num_1+num_2)
