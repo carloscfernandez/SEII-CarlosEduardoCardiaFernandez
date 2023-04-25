5472b2017831d1080210df5b0e1987f83ff64d9d
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-04-25 11:24:37 -0300

    Create py06.py

---
+++ SEMANA02/Exercicio04/py06.py
@@ -0,0 +1,42 @@
+#Object Identy: is          id(a)==id(b)
+#and
+#or
+#not
+
+#False values: False, None, Zero of any numeric type, Any empty sequence '', (), [], {}
+
+
+condition=[]
+
+
+if condition:
+    print('Evalueted to True')
+else:
+    print('Evalueted to False')
+
+
+
+
+
+
+
+
+#user ='Admin'
+#logged_in=False
+
+#if not logged_in:
+ #   print('Please log in')
+#else:
+ #   print('Welcome')
+
+
+#language = 'Java'
+
+#if language == 'Python':
+ #   print('Language is Python')
+#elif language == 'Java':
+ #   print('Language is Java')
+#elif language == 'JavaScript':
+ #   print('Language is JavaScript')
+#else:
+ #   print('No match')
