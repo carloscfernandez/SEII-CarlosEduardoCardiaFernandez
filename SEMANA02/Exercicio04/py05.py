a2c4af7985a89c25476301ec11fc57f458fa1088
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-04-25 11:08:37 -0300

    Create py05.py

---
+++ SEMANA02/Exercicio04/py05.py
@@ -0,0 +1,15 @@
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'COmpSci']}
+#age = student.pop('age')
+#del student['age']
+#student.update({'name': 'Jane', 'age': 26, 'phone': '555-555'})
+#student['phone']='555-555'
+#student['name']='Jane'
+#print(student['courses'])
+#print(student.get('name', 'Not found'))
+#print(len(student))
+#print(student.keys())
+#print(student.values())
+#print(student.items())
+
+for key, value in student.items():
+    print(key, value)
