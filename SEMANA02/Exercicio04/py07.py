91e9e6236c4cfc0d52f265aa75b7733ee6bfcc2a
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-04-25 11:33:59 -0300

    Create py07.py

---
+++ SEMANA02/Exercicio04/py07.py
@@ -0,0 +1,21 @@
+#break sai do condicional
+#continue Faz a ação e continua no for
+
+#nums=[1, 2, 3, 4, 5]
+
+#for num in nums:
+ #   for letter in 'abc':
+  #      print(num, letter)
+
+
+#for i in range(1, 11):
+ #   print(i)
+
+
+
+x=0
+while True:
+    if x == 5:
+        break
+    print(x)
+    x+=1
