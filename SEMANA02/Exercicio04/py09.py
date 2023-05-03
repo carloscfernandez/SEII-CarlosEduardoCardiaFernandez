df9306d10d9c90b79a5c1c6ad1978f84f155437c
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-03 17:23:53 -0300

    Create py09.py

---
+++ SEMANA02/Exercicio04/py09.py
@@ -0,0 +1,37 @@
+#import my_module as mm
+#from my_module import *
+
+#import sys
+
+#from my_module import find_index, test
+
+#index = mm.find_index(courses, 'Math')
+#index = find_index(courses, 'Math')
+
+#print(index)
+#print(test)
+
+#print(sys.path)
+
+#import random
+#random_course=random.choice(courses)
+#print(random_course)
+
+#import math
+#rads=math.radians(90)
+#print(rads)
+#print(math.sign(rads))
+
+#import datetime
+#import calendar
+
+#today = determinate.date.today()
+#print(today)
+#print(calendar.isleap(2017))
+
+#import os
+#print(os.getcwd())
+#print(os.__file__)
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
