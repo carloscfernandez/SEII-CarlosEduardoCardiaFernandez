ce28ea83e0602d8895b9283da26369d4b76da046
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-03 17:47:24 -0300

    Create py11.py

---
+++ SEMANA02/Exercicio04/py11.py
@@ -0,0 +1,25 @@
+
+#f = open('test.txt', 'r')
+#print(f.name)
+#f.close()
+
+#with open('test.txt', 'r') as f:
+#    size_to_read=100
+ #   f_contens = f.readlines(size_to_read)
+  #  while len(f_contens>0:
+   #           print(f_contents)
+    #          f_contens = f.readlines(size_to_read)
+
+
+  #  f_contens = f.read(100)
+   # print(f_contents, end='')
+
+ #  with open('test.txt', 'w') as f:
+   #    f.write('Test')
+    #   f.seek(0)
+     #  f.write('R')
+
+with open('test.txt', 'r') as f:
+    with open('test.txt', 'w') as f:
+        for line in rf
+        wf.write(line)
