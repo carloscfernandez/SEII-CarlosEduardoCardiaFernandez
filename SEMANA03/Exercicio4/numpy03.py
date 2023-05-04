5f8f6d1a5a7b2aa5d839b4ded7883b07fa5b1386
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-04 01:14:57 -0300

    Create numpy03.py

---
+++ SEMANA03/Exercicio4/numpy03.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1,2,3])
+print(a)
+
+print(a[0])
+a[0]=10
+print(a)
+
+b=a*np.array([2,0,2])
+print(b)
+
+
+#print(a.shape)
+#print(a.dtype)
+#print(a.ndim)
+#print(a.itemsize)
