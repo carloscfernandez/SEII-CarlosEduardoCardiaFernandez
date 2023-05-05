1778288814d919058692071a1060605a0331f097
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-05 15:23:21 -0300

    Create file06.py

---
+++ SEMANA04/Exercicio2/file06.py
@@ -0,0 +1,14 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        print(name)
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+
+d=Dog("Tim",34)
+print(d.name)
+d2=Dog("Bill",12)
+print(d2.name)
