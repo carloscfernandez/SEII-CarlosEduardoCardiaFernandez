5b45db59f552a6fb052a6f4a2ee519c7d5026fa4
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-05 15:27:59 -0300

    Create file08.py

---
+++ SEMANA04/Exercicio2/file08.py
@@ -0,0 +1,16 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        print(name)
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    def set_age(self,age):
+        self.age = age
+dog1_name="Tim"
+dog1_age=34
+
+dogs_name=["Tim", "Bill"]
+dogs_age=[32,13]
