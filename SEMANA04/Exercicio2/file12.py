6be26d5dfd6855ed47c1654990a26d0fc92e135c
carloscfernandez <89081940+carloscfernandez@users.noreply.github.com>
2023-05-05 15:55:38 -0300

    Create file12.py

---
+++ SEMANA04/Exercicio2/file12.py
@@ -0,0 +1,24 @@
+class Person:
+    number_of_people=0
+    gravity=-9.8
+    def __init__(self,name):
+        self.name=name
+        Person.add_person()
+       # Person.number_of_people+=1
+
+    @classmethod   
+    def number_of_people_(cls):
+        return cls.number_of_people()
+
+    @classmethod   
+    def add_person(cls):
+        cls.number_of_people+=1
+
+p1=Person("tim")
+print(Person.number_of_people)
+p2=Person("jill")
+print(Person.number_of_people)
+
+#Person.number_of_people=8
+#print(p2.number_of_people)
+#print(p1.number_of_people)
